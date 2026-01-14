9db1f0b4348cad40eda7376b0f0f0a0c93e775b6
jcardas <justincardas@gmail.com>
2026-01-13 20:48:54 -0800

    Refactor labs folder

---
+++ COMP4537/labs/0/lang/messages/en/user.js
@@ -0,0 +1,7 @@
+const SUCCESS_MESSAGE = "Excellent memory!"
+const FAIL_MESSAGE = "Wrong order!"
+
+const OUT_OF_RANGE_ERROR = "Please enter a number between 3 and 7."
+
+const PROMPT_MESSAGE = "How many buttons to create?"
+const GO_BUTTON_MESSAGE = "Go!"
